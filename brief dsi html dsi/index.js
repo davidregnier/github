3dab6521d7734b1ea54319668ff3368cb73b8fbd
EpicMaDneSss <120367585+EpicMaDneSss@users.noreply.github.com>
2023-02-20 16:35:16 +0100

    final?

--- brief dsi html dsi/index.js
+++ brief dsi html dsi/index.js
@@ -1,7 +1,7 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const menuBurger = document.querySelector('.menu-burger');
+// const burgerMenu = document.querySelector('.burger-menu');
+// const menuBurger = document.querySelector('.menu-burger');
 
-burgerMenu.addEventListener('click', () => {
-  menuBurger.classList.toggle('show');
-  burgerMenu.classList.toggle('active');
-});
+// burgerMenu.addEventListener('click', () => {
+//   menuBurger.classList.toggle('show');
+//   burgerMenu.classList.toggle('active');
+// });
