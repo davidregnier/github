4c389a045d8ee2f5e7b0b3360940f7d5ef762eb6
EpicMaDneSss <120367585+EpicMaDneSss@users.noreply.github.com>
2023-03-28 10:12:52 +0200

    ok

--- brief dsi html dsi/index.js
+++ brief dsi html dsi/index.js
@@ -8,31 +8,115 @@
 //   burgerMenu.classList.toggle('active');
 // });
 
-//carrousel à modifier////////////////////////////////////
+//  // Sélection des éléments HTML
+//  const carousel = document.querySelector('.diagrame');
+//  const images = carousel.querySelectorAll('.diagrame1 img');
+//  const prevButton = document.createElement('button');
+//  const nextButton = document.createElement('button');
 
-// document.body.onload=function(){
-//     let nbr=5;
-//     let p=0;
-//     container=document.getElementById("container");
-//     r=document.getElementById("r");
-//     l=document.getElementById("l");
-//     container.style.width=(800*nbr)+"px";
-//     for (i = 1; i <=nbr ; i++) {
-//         div=document.createElement("div");
-//         div.className="photo";
-//         div.style.backgroundImage="url('IMG/im"+i+".jpg')";
-//         container.appendChild(div);
-//     }
-//     l.onclick=function () {
-//         if (p>-nbr+1)
-//         p--;
-//         container.style.transform="translate("+p*800+"px)";
-//         container.style.transition="all 0.5s ease";
-//     }
-//     r.onclick=function () {
-//         if(p<0)
-//         p++;
-//         container.style.transform="translate("+p*800+"px)";
-//     }
-// }
+//  // Configuration des boutons "précédent" et "suivant"
+//  prevButton.textContent = 'Précédent';
+//  nextButton.textContent = 'Suivant';
 
+//  // Ajout des boutons au HTML
+//  carousel.appendChild(prevButton);
+//  carousel.appendChild(nextButton);
+
+//  // Variables pour le suivi de l'image actuelle
+//  let currentImage = 0;
+//  const maxImages = images.length - 1;
+
+//  // Fonction pour masquer toutes les images sauf celle actuelle
+//  function hideAllImages() {
+//      for (let i = 0; i <= maxImages; i++) {
+//          images[i].style.display = 'none';
+//      }
+//  }
+
+//  // Affichage initial de la première image
+//  hideAllImages();
+//  images[currentImage].style.display = 'block';
+
+//  // Fonction pour passer à l'image suivante
+//  function nextImage() {
+//      hideAllImages();
+//      currentImage++;
+//      if (currentImage > maxImages) {
+//          currentImage = 0;
+//      }
+//      images[currentImage].style.display = 'block';
+//  }
+
+//  // Fonction pour passer à l'image précédente
+//  function prevImage() {
+//      hideAllImages();
+//      currentImage--;
+//      if (currentImage < 0) {
+//          currentImage = maxImages;
+//      }
+//      images[currentImage].style.display = 'block';
+//  }
+
+//  // Ajout des écouteurs d'événements pour les clics sur les boutons
+//  prevButton.addEventListener('click', prevImage);
+//  nextButton.addEventListener('click', nextImage);
+ // Sélection des éléments HTML
+ const carousel = document.querySelector('.diagrame');
+ const images = carousel.querySelectorAll('.diagrame1 img');
+ const prevButton = document.createElement('button1');
+ const nextButton = document.createElement('button2');
+
+ // Configuration des boutons "précédent" et "suivant"
+ prevButton.textContent = '<';
+ nextButton.textContent = '>';
+
+ // Ajout des boutons au HTML
+ carousel.appendChild(prevButton);
+ carousel.appendChild(nextButton);
+
+ // Variables pour le suivi de l'image actuelle
+ let currentImage = 0;
+ const maxImages = images.length - 1;
+
+ // Fonction pour masquer toutes les images sauf celle actuelle
+ function hideAllImages() {
+     for (let i = 0; i <= maxImages; i++) {
+         images[i].style.display = 'none';
+     }
+ }
+
+ // Affichage initial de la première image
+ hideAllImages();
+ images[currentImage].style.display = 'block';
+
+ // Fonction pour passer à l'image suivante
+ function nextImage() {
+     hideAllImages();
+     currentImage++;
+     if (currentImage > maxImages) {
+         currentImage = 0;
+     }
+     images[currentImage].style.display = 'block';
+ }
+
+ // Fonction pour passer à l'image précédente
+ function prevImage() {
+     hideAllImages();
+     currentImage--;
+     if (currentImage < 0) {
+        currentImage = maxImages;
+    }
+    images[currentImage].style.display = 'block';
+}
+
+// Ajout des écouteurs d'événements pour les boutons
+prevButton.addEventListener('click', prevImage);
+nextButton.addEventListener('click', nextImage);
+
+// Fonction pour passer automatiquement à l'image suivante toutes les 5 secondes
+function autoNextImage() {
+    setInterval(nextImage, 500);
+}
+
+// Appel de la fonction pour passer automatiquement à l'image suivante
+autoNextImage();
